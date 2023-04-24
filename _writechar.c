148e5e9481c043878de027589c399f26aa1d2d39
Themby2000 <sthembie@gmail.com>
2023-04-24 12:45:16 -0700

    Printf1.c

---
+++ _writechar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * _writechar - writes the character c to stdout
+ * @c: The character to write
+ *
+ * Return: 1 on success and 01 on error
+ */
+int _writechar(char c)
+{
+	return (buffer(c));
+}
