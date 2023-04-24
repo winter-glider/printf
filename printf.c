38390bc1304fcab7e879831162d81d50fd73bb1f
winter-glider <nyeveratanatswa04@gmail.com>
2023-04-24 07:00:29 -0700

    task 0

--- printf.c
+++ printf.c
@@ -13,43 +13,34 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	if (format == NULL)
-		return (- 1);
-    va_start(args, format);
-    while (format[i] != '\0')
-    { 
-	    if (format[i] == '%' && format[i + 1] == '\0')
-	    {
-		    return (- 1);
-	    }
-	    if (format[i] == '%' && format[i + 1] != '\0')
-	    {
-		    i++;
-		    if (format[i] == 'c')
-		    {
-			    c = va_arg(args, int);
-			    total += write_char(c);
-		    }
-		    else if (format[i] == 's')
-		    {
-			    s = va_arg(args, char *);
-			    if (s == NULL)
-			    {
-				    s = "(null)";
-			    }
-			    total += print(s);
-		    }
-		    else if (format[i] == '%')
-		    {
-			    total += write_char('%');
-		    }
-	    }
-	    else
-		    total += write_char(format[i]);
-	    i++;
-    }
-    va_end(args);
-    return(total);
+		return (-1);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				total += write_char(c);
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				total += print(s);
+			}
+			else if (format[i] == '%')
+				total += write_char('%');
+		}
+		else
+			total += write_char(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (total);
 }
-
-
-
